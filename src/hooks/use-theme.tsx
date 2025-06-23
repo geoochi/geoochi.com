c4d6efc616e017b49f10feb323f8b982f28d7601
Geoo Chi <geoochi@geoochi.com>
2025-06-24 07:48:34 +0800

    update

--- src/hooks/use-theme.tsx
+++ src/hooks/use-theme.tsx
@@ -4,17 +4,15 @@ function useTheme() {
   const [theme, setTheme] = useState('')
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem('theme')
-    if (savedTheme) {
-      setTheme(savedTheme)
-    } else {
-      setTheme(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
-    }
+    setTheme(
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'dark'
+        : 'light'
+    )
   }, [])
 
   useEffect(() => {
     if (!theme) return
-    localStorage.setItem('theme', theme)
     if (theme === 'dark') {
       document.documentElement.classList.add('dark')
     } else {
