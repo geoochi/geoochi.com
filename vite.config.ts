e547a939a8991db7244959ed92d348089611012d
Geoo Chi <geoochi@geoochi.com>
2025-07-31 13:52:32 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
-import { execSync } from 'child_process'
 
 export default defineConfig({
   resolve: {
@@ -15,19 +14,5 @@ export default defineConfig({
         renderTarget: '#root',
       },
     }),
-    {
-      name: 'generate-sitemap-after-build',
-      apply: 'build',
-      closeBundle() {
-        // 你可以自定义子目录
-        const subPages = ['blog', 'gallery']
-        try {
-          execSync(`bun scripts/generate-sitemap.ts ${subPages.join(' ')}`, { stdio: 'inherit' })
-          console.log('Sitemap generated after build.')
-        } catch (e) {
-          console.error('Failed to generate sitemap:', e)
-        }
-      },
-    },
   ],
 })
