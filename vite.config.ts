1630a88b842ab3649f41307bc14baa1e04c3dc7d
Geoo Chi <geoooochi@gmail.com>
2025-12-18 10:38:53 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 export default defineConfig({
+  base: 'https://singfor-live-9gizzbuc59a85fe6-1314828645.tcloudbaseapp.com/space/',
   resolve: {
     alias: {
       '@': '/src',
